b1519677a0376de620013efce007ae873ac93a27
=sourav singh kunwar <souravsinghkunwar1@gmail.com>
2025-02-03 17:09:20 +0530

    little bit of dom manipulation

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+let s = "my name is sourav";
+
+console.log(s.includes("is sourav"));
+console.log(s.substring(0,s.length));
+
+let heading = document.getElementById("myheading");
+
+heading.innerText = s;
+heading.style.backgroundColor="red";
