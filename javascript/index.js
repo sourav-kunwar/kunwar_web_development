2414d61d59aa5958bb35a2dfbf4879f1de81fed4
=sourav singh kunwar <souravsinghkunwar1@gmail.com>
2025-02-04 16:32:03 +0530

    new page

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,61 @@
-let s = "my name is sourav";
+// if else, switch ad loops
+// let icecream = Number(prompt("type a number"));
 
-console.log(s.includes("is sourav"));
-console.log(s.substring(0,s.length));
+// console.log(icecream);
 
-let heading = document.getElementById("myheading");
+// if(icecream == 10)
+// {
+//     console.log("vanilla icecream");
+// }
+// else if(icecream == 20)
+// {
+//     console.log("strawberry icecream");
+// }
+// else if(icecream<10)
+// {
+//     console.log("hawa khaa");
+// }
+// else{
+//     console.log("aap konsa du apko")
+// }
 
-heading.innerText = s;
-heading.style.backgroundColor="red";
+
+// // switch case:
+
+
+// switch (icecream) {
+//     case 10:
+//         console.log("vanilla icecream");
+//         break;
+//     case 20:
+//         console.log("strawberry icecream");
+//     default:
+//         break;
+// }
+
+
+// for loop
+
+// for(let i=1; i<=10; i=i+1)
+// {
+//     console.log(2*i);
+//     console.log(i);
+// }
+
+// while loop
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(3*i);
+//     i++;
+// }
+
+// do-while loop
+
+
+let i = 1;
+
+ do{
+     console.log(5*i);
+        i++;
+ }while(i<=10)
